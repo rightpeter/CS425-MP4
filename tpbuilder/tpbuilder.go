cb18f914b3bc987c7609556c447f0995806b0a40
Rightpeter <rightpeter.lu@gmail.com>
2018-11-30 17:50:35 -0600

    Fix

--- tpbuilder/tpbuilder.go
+++ tpbuilder/tpbuilder.go
@@ -24,29 +24,34 @@ type TpBuilder struct {
 }
 
 // NewTpBuilder new tpBuilder
-func NewTpBuilder(ip string, port int) TpBuilder {
-	tpBuilder := TpBuilder{}
+func NewTpBuilder(ip string, port int) *TpBuilder {
+	tpBuilder := &TpBuilder{}
 	tpBuilder.ip = ip
 	tpBuilder.port = port
+	builder := Builder{}
+	builder.Spout = map[string]spout.Builder{}
+	builder.Bolt = map[string]bolt.Builder{}
+
+	tpBuilder.builder = builder
 
 	return tpBuilder
 }
 
 // SetSpout set spout
-func (t TpBuilder) SetSpout(id string, sp spout.Spout, parallel int) spout.Builder {
+func (t *TpBuilder) SetSpout(id string, sp spout.Spout, parallel int) spout.Builder {
 	spoutBuilder := spout.Builder{Spout: sp, Parallel: parallel}
 	t.builder.Spout[id] = spoutBuilder
 	return spoutBuilder
 }
 
 // SetBolt set bolt
-func (t TpBuilder) SetBolt(id string, bt bolt.Bolt, parallel int) bolt.Builder {
+func (t *TpBuilder) SetBolt(id string, bt bolt.Bolt, parallel int) bolt.Builder {
 	boltBuilder := bolt.Builder{ID: id, Bolt: bt, Parallel: parallel, Grouping: map[string]model.GroupingType{}}
 	t.builder.Bolt[id] = boltBuilder
 	return boltBuilder
 }
 
-func (t TpBuilder) callSubmitRPC(client *rpc.Client, streamID string) error {
+func (t *TpBuilder) callSubmitRPC(client *rpc.Client, streamID string) error {
 	t.builder.ID = streamID
 	err := client.Call("Crane.RPCSubmitStream", &t.builder, nil)
 	if err != nil {
@@ -56,7 +61,7 @@ func (t TpBuilder) callSubmitRPC(client *rpc.Client, streamID string) error {
 }
 
 // Submit submit
-func (t TpBuilder) Submit(streamID string) error {
+func (t *TpBuilder) Submit(streamID string) error {
 	client, err := rpc.DialHTTP("tcp", fmt.Sprintf("%s:%d", t.ip, t.port))
 	if err != nil {
 		log.Fatal("dialing:", err)
