eb94a73564d56f83e643a7c02be167e5208e890e
Rightpeter <rightpeter.lu@gmail.com>
2018-12-01 16:03:23 -0600

    Fix

--- worker/worker.go
+++ worker/worker.go
@@ -175,6 +175,7 @@ func (w *Worker) RPCExecuteTask(task model.BoltTuple, reply *bool) error {
 	if _, ok := w.boltChannels[task.ID]; !ok {
 		return fmt.Errorf("no channel for bolt: %v", task.ID)
 	}
+	log.Printf("ass task %v to bolt %s's channel", task.UUID, task.ID)
 	w.boltChannels[task.ID] <- task
 	return nil
 }
