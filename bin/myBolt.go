bbbda8ac3c5c62e60e679257dea5dbd8e302df60
Rightpeter <rightpeter.lu@gmail.com>
2018-12-01 16:57:08 -0600

    Fix

--- bin/myBolt.go
+++ bin/myBolt.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	args := os.Args
 	if len(args) > 1 {
-		fmt.Println(args[1])
+		words := strings.Fields(args[1])
+		for _, word := range words {
+			fmt.Println(word)
+		}
 	} else {
 		fmt.Println("No input")
 	}
