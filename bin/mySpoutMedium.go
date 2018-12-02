344833082ec95fac70f7540db092a1cd97249ab7
Rightpeter <rightpeter.lu@gmail.com>
2018-12-01 20:23:20 -0600

    Fix

---
+++ bin/mySpoutMedium.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	//"time"
+)
+
+func main() {
+	file, err := os.Open("/tmp/medium.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
